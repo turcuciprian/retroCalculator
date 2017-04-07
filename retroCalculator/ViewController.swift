dfbb1089e9325c0d74e3ac6885a96b000a6659c3
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-08 00:41:33 +0300

    Added ibActions and made all buttons functional trouble is, operations are not working. last bug is making operations work

--- retroCalculator/ViewController.swift
+++ retroCalculator/ViewController.swift
@@ -14,7 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var outputLabel: UILabel!
     var btnSound: AVAudioPlayer!
     
+    enum Operation: String{
+        case Divide = "/"
+        case Multiply = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    var currentOperation = Operation.Empty
     var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    
 
     
     override func viewDidLoad() {
@@ -30,13 +42,31 @@ class ViewController: UIViewController {
         } catch let err as NSError{
             print(err.debugDescription)
         }
+        
+        outputLabel.text = "0"
     }
     @IBAction func numberPressed (sender: UIButton){
         playSound()
         
         runningNumber += "\(sender.tag)"
         outputLabel.text = runningNumber
-        
+    }
+    
+    @IBAction func onDividePressed (sender: AnyObject){
+        processOperation(operation: .Divide)
+    }
+    @IBAction func onMultiplyPressed (sender: AnyObject){
+        processOperation(operation: .Multiply)
+    }
+    @IBAction func onSubstractPressed (sender: AnyObject){
+        processOperation(operation: .Substract)
+    }
+    @IBAction func onAddPressed (sender: AnyObject){
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject){
+        //processOperation(operation: currentOperation)
     }
     
     func playSound(){
@@ -44,6 +74,32 @@ class ViewController: UIViewController {
             btnSound.stop()
         }else{
             btnSound.play()
+        }
+    }
+    func processOperation(operation: Operation){
+        playSound()
+        if currentOperation != Operation.Empty{
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply{
+                    result = " \(Double(leftValStr)! * Double(rightValStr)!)"
+                }else if currentOperation == Operation.Divide{
+                    result = " \(Double(leftValStr)! / Double(rightValStr)!)"
+                }else if currentOperation == Operation.Substract{
+                    result = " \(Double(leftValStr)! - Double(rightValStr)!)"
+                }else if currentOperation == Operation.Add{
+                    result = " \(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                leftValStr = result
+                outputLabel.text = result
+        }
+            currentOperation = operation
+        } else{
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
         }
     }
 
