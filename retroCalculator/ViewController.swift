5747d9bc99cc6feac300dcd7553a00266083f688
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-07 23:36:21 +0300

    added ibaction to all buttons, renamed all buttons

--- retroCalculator/ViewController.swift
+++ retroCalculator/ViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var btnSound: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundUrl = URL(fileURLWithPath: path!)
+        do{
+            
+         try btnSound = AVAudioPlayer(contentsOf: soundUrl)
+            btnSound.prepareToPlay()
+            
+        } catch let err as NSError{
+            print(err.debugDescription)
+        }
+    }
+    @IBAction func numberPressed (sender: UIButton){
         
+    }
+    func playSound(){
         
     }
 
